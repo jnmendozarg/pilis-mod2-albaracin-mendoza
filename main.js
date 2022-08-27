9fdd3e2a19f42cc24a99af432ad0074c8b5eeeba
Nicolas Mendoza <jnmendozarg@gmail.com>
2022-08-27 01:01:25 -0300

    version final

--- main.js
+++ main.js
@@ -1,31 +1,68 @@
-// Al hacer scroll en la pagina se ejecuta esta funcion:
-window.onscroll = function() {myFunction()};
+//API clima
+let temperaturaValor = document.getElementById('temperatura-valor')
+let temperaturaDescripcion = document.getElementById('temperatura-description')
+let ubicacion = document.getElementById('ubicacion')
+let iconoAnimado = document.getElementById('icono-animado')
 
-// Identificar a la barra de navegacion
-var navbar = document.getElementById("navbar");
+let lat = -24.183388;
+let lon = -65.331502;
+let API_KEY= "b856f7ee0ff958361a8997be08cac1bb";
+const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&lang=es&units=metric&appid=${API_KEY}`;
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+fetch(url)
+  .then( response => {return response.json()})
+  .then( data => {
+   
+    let temp = Math.round(data.current.temp);
+    temperaturaValor.textContent  = `${temp} °C`;
+  
+    let description = data.current.weather[0].description;
+    temperaturaDescripcion.textContent   = description.toUpperCase();
 
-// Dar la clase sticky en la navbar cuando se esta haciedno el scroll. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
+    let iconCode = data.current.weather[0].icon;
+    const urlIcon = `http://openweathermap.org/img/wn/${iconCode}.png`
+    icono.src = urlIcon;
+  })
+  .catch( error => {
+    console.log(error);
+  }) 
+ 
+
+
+async function saveData(event) {
+    event.preventDefault();
+    const message = {
+      business : document.getElementById('business').value,
+      name : document.getElementById('name').value,
+      phone : document.getElementById('phone').value
+    }
+    console.log(message);
+    try{
+      let response = await fetch ("https://jsonplaceholder.typicode.com/posts",{
+        method: 'POST',
+        body: JSON.stringify(message),
+        headers: { "Content-type": "application/json; charset=UTF-8" }
+      })
+        let data = await response.json()
+        console.log(data)
+        Swal.fire({
+          icon:'success',
+          title: 'Mensaje enviado',
+          showConfirmButton: false,
+          timer: 1500
+        });
+        cleanForm();
+      }catch(error){
+      console.log(error)
+    }
 }
 
+function cleanForm() {
+  let formulario = document.getElementById('formulario');
+  formulario.reset();
+}
 
-//El evento load se dispara cuando un recursos y sus recursos dependientes terminaron de cargar  
-//window.addEventListener('load',() =>{})
-https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=-24.183388&lon=-65.331502&dt={time}&appid=b856f7ee0ff958361a8997be08cac1bb
+let boton = document.getElementById("enviar");
+boton.addEventListener("click", saveData);
 
-lat = -24.183388; //al poner let me dice problemas en consola
-lon = -65.331502;
-API_KEY= "b856f7ee0ff958361a8997be08cac1bb";
 
-//https://api.openweathermap.org/data/2.5/onecall?lat=-24.183388&lon=-65.331502&exclude={part}&appid=b856f7ee0ff958361a8997be08cac1bb
-
-const url_base = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`;
-console.log(url_base);
